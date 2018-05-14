1b566f1643c63e2a7c42a1e0c97ff432591c5d02
lporter <porterlogan@gmail.com>
2018-05-14 10:04:09 -0400

    Created the base JavaFX application

--- StudyBitProject/src/Main/Main.java
+++ StudyBitProject/src/Main/Main.java
@@ -1,16 +1,8 @@
-/**
- Main.java
-
- Original Module: Logan Porter
-
- Date: 05-12-2018
-
- */
-
-
 package Main;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
